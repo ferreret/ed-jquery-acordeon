410294d94caff298228aa68e88e24f2c02ee74f6
ferreret <ferreret@gmail.com>
2015-07-15 16:51:24 +0200

    Fin del ejemplo

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-$(document).ready(function () {
+/* $('.acordeon__titulo').on('click', function() {	
+	// var $contenido = $(this).siblings('.acordeon__contenido')
+	var $contenido = $(this).next();
+
+	$contenido.toggle('easing');
+	$('.acordeon__contenido').not($contenido).hide('easing');
+}); */
+
+$('.acordeon').on('click', 'h2', function() {	
+	var $t = $(this);
+	var $tp = $t.next();
+	var $p = $t.parent().siblings().find('p');
 	
-});
+    $tp.slideToggle();
+    $p.slideUp();
+})
